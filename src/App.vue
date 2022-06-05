a0f3b388dc1237a34266a088f04f99359182859d
NourEldin Ahmed Samir <NourEldin20201895@fci.helwan.edu.eg>
2022-06-05 16:27:13 +0200

    Finishing up components

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,91 @@
     <div class="container">
       <h2>IP Address Tracker</h2>
       <div class="input-field">
-        <input type="text" placeholder="Enter an IP Address or press ENTER" />
+        <input
+          type="text"
+          placeholder="Enter an IP Address or press ENTER"
+          v-model="query"
+          @keyup.enter="fetchIPInfo"
+        />
         <span class="icon">
           <img src="./assets/arrow-right.svg" alt="" srcset="" />
         </span>
       </div>
-      <!-- Card component goes here -->
+      <IPCard :IPInfo="IPInfo" v-if="IPInfo" />
     </div>
     <div id="map"></div>
   </section>
 </template>
 
 <script>
+import IPCard from "./components/IPCard.vue";
+import { onMounted, ref } from "vue";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+import marker from "./assets/marker.svg";
+
 export default {
   name: "App",
-  components: {}
-}
+  components: { IPCard },
+  setup() {
+    let myMap;
+    let query = ref("");
+    let IPInfo = ref(null);
+
+    let markerIcon = L.icon({
+      iconUrl: marker,
+      iconSize: [23, 28],
+      iconAnchor: [16, 17],
+    });
+
+    // Loading map
+    onMounted(() => {
+      let lng = 31.308952111,
+        lat = 30.10093501241242;
+      let myHomeLngLat = [lat, lng];
+
+      myMap = L.map("map").setView(myHomeLngLat, 18);
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(
+        myMap
+      );
+    });
+
+    // Fetching IPInfo
+    async function fetchIPInfo() {
+      try {
+        let response = await fetch(
+          `https://geo.ipify.org/api/v1?apiKey=at_RBfgufKhpizoxKa5DY66rJqyW6VVx&ipAddress=${query.value}`
+        );
+
+        let result = await response.json();
+
+        IPInfo.value = {
+          address: result.ip,
+          state: result.location.city,
+          timezone: result.location.timezone,
+          isp: result.isp,
+          lat: result.location.lat,
+          lng: result.location.lng,
+        };
+
+        // Updating map view & marker
+        myMap.setView([IPInfo.value.lat, IPInfo.value.lng], 13);
+        L.marker([IPInfo.value.lat, IPInfo.value.lng], {
+          icon: markerIcon,
+        }).addTo(myMap);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    return {
+      query,
+      IPInfo,
+      fetchIPInfo,
+    };
+  },
+};
 </script>
 
 <style>
@@ -28,6 +97,7 @@ export default {
   --clr-white: hsl(180, 0%, 95%);
   --clr-black: hsl(180, 0%, 10%);
   --clr-grey: hsl(180, 0%, 35%);
+  --shadow: hsla(180, 0%, 10%, 50%);
   --ff-text: "Inter", sans-serif;
 }
 
@@ -52,6 +122,7 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+  position: relative;
 }
 
 .container > h2 {
@@ -86,6 +157,14 @@ img {
   width: auto;
   height: 100%;
   object-fit: cover;
+}
+
+#map * {
+  z-index: 2;
+}
+
+.leaflet-bottom.leaflet-right {
+  display: none;
 }
 
 @media all and (max-width: 767px) {
---
+++ src/components/IPCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="IPCard">
+    <div id="address">
+      <small>IP Address</small>
+      <h3>{{ IPInfo.address }}</h3>
+    </div>
+    <div id="state">
+      <small>Location</small>
+      <h3>{{ IPInfo.state }}</h3>
+    </div>
+    <div id="timezone">
+      <small>Timezone</small>
+      <h3>{{ IPInfo.timezone }}</h3>
+    </div>
+    <div id="isp">
+      <small>ISP</small>
+      <h3>{{ IPInfo.isp }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IPCard",
+  props: ["IPInfo"],
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.IPCard {
+  z-index: 4;
+  position: absolute;
+  background: var(--clr-white);
+  box-shadow: 0px 0px 16px var(--shadow);
+  border-radius: 0.5rem;
+
+  bottom: -4em;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 2em;
+  padding: 2em 5em;
+}
+
+small {
+  color: var(--clr-grey);
+}
+
+@media all and (max-width: 725px) {
+  .IPCard {
+    display: flex;
+    flex-direction: column;
+    width: 60%;
+    gap: 0.75em;
+    bottom: -13rem;
+    padding: 2em;
+  }
+}
+</style>
